7ffdf1e78802eb28e1026a39ca9fd896415aa1ea
kavyasp20 <149318455+kavyasp20@users.noreply.github.com>
2024-11-17 12:28:07 +0530

    Create ticketfever.py

---
+++ visa_prep/ticketfever.py
@@ -0,0 +1,10 @@
+T = int(input())
+for _ in range(T):
+    N,M = map(int,input().split())
+    
+    for i in range(T):
+        m = N-M
+    if m>0:
+        print(m)
+    else:
+        print("0")
